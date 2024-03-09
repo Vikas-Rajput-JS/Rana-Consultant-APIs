7277b090891d9c4ce561779a8db952bb6bef305e
davidthakur <vr384695@gmail.com>
2024-03-10 00:36:44 +0530

    changes

--- app.js
+++ app.js
@@ -4,25 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors')
+
+const CommonAPI = require('./routes/CommonAPI')
+const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const UserRouter = require('./routes/users')
+const UserRouter = require('./routes/users');
+
 require('./Connection/DB')
 var app = express();
 app.use(cors())
+app.use(bodyParser())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+// app.use(fileUpload())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images/modal=user',express.static(path.join(__dirname, 'public/data/uploads')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/',UserRouter)
+app.use('/common',CommonAPI)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
