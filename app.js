e23efa6bbbe97c3c6cc899df7ef565c8c5037e82
davidthakur <vr384695@gmail.com>
2024-03-13 23:04:18 +0530

    changes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const UserRouter = require("./routes/users");
 const AdminRouter = require("./AdminRoutes/User");
+const CourseRouter = require("./routes/Courses");
 require("./Connection/DB");
 var app = express();
 app.use(cors());
@@ -34,6 +35,7 @@ app.use("/", indexRouter);
 app.use("/", UserRouter);
 app.use("/common", CommonAPI);
 app.use("/admin", AdminRouter);
+app.use("/", CourseRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
